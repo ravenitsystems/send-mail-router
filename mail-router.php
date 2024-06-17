65dac428582a277d6ad008cbd9f5579ae9569499
Nicholas Ashley <nicholas.ashley@freetimers.com>
2024-06-17 16:26:32 +0100

    Put basic log in

--- mail-router.php
+++ mail-router.php
@@ -1,6 +1,18 @@
 <?php
 
+require(__DIR__ . '/vendor/autoload.php');
+
+$log_directory = '/var/log/mail-router';
+if (!file_exists($log_directory)) {
+    @mkdir($log_directory);
+}
+if (!file_exists($log_directory) || !is_dir($log_directory)) {
+    throw new Exception("Could not create log directory $log_directory");
+}
+
+
+
 $stdin = file_get_contents('php://stdin');
 
-file_put_contents('outlog.txt', $stdin, FILE_APPEND);
+file_put_contents($log_directory . '/' . time() . '.msg', $stdin, FILE_APPEND);
 
